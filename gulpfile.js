0757b52a9c9a8a4cd60ca5670b0d4e1a9d146bd0
Amaury Hanser <amaury.hanser@prestashop.com>
2021-01-21 15:33:42 +0100

    Improvements: MJML4, Gulp4, Multi-themes

--- gulpfile.js
+++ gulpfile.js
@@ -8,130 +8,201 @@ var replace_task = require('gulp-replace-task');
 var ext_replace = require('gulp-ext-replace');
 var zip = require('gulp-zip');
 var replace = require('gulp-replace');
-var rename = require("gulp-rename");
 var fs = require('fs');
 var del = require('del');
+var through = require('through2');
+var migrate = require('mjml-migrate').default;
+var htmlBeautify = require('js-beautify').html;
+const path = require('path');
 
-var settings = require('./src/config/settings.json');
+// Get all folders in src/
+function getFolders(dir) {
+  return fs.readdirSync(dir)
+    .filter(function(file) {
+      return fs.statSync(path.join(dir, file)).isDirectory();
+    });
+}
 
-// Compile files [dev mode]
-gulp.task('build:dev', function () {
-    var css = fs.readFileSync(__dirname+'/src/css/global.css', 'utf8');
-    var fake_json_path = './src/config/fake.json';
-    delete require.cache[require.resolve(fake_json_path)]
-    var fake = require(fake_json_path);
 
-    return gulp.src(['src/*.mjml'])
+const themes = getFolders('src');
 
-        // Compile MJML to HTML
-        .pipe(mjml(mjmlEngine))
+themes.forEach(function(theme) {
+  gulp.task(`build:dev:${theme}`, function () {
 
-        // Delete conditions
-        .pipe(replace('{{/if}}', ''))
-        .pipe(replace(/{{if \$[a-z_]+}}/ig, ''))
+    var css = fs.readFileSync(__dirname+`/src/${theme}/css/global.css`, 'utf8');
+      var fake_json_path = `./src/${theme}/config/fake.json`;
+      delete require.cache[require.resolve(fake_json_path)]
+      var fake = require(fake_json_path);
 
-        // We need to decode some curly brackets & dollar signs because of MJML
-        .pipe(replace('%7B%7B', '{{'))
-        .pipe(replace('%7D%7D', '}}'))
-        .pipe(replace('&#36;', '$'))
+      return gulp.src([`src/${theme}/*.mjml`])
 
-        // CSS injection in the header
-        .pipe(replace('</head>', '<style>'+css+'</style></head>'))
+          // Compile MJML to HTML
+          .pipe(mjml(mjmlEngine))
 
-        // Translate
-        .pipe(i18n({
-            langDir: './langs',
-            trace: true,
-            createLangDirs: true
-        }))
+          // Delete conditions
+          .pipe(replace('{{/if}}', ''))
+          .pipe(replace(/{{if \$[a-z_]+}}/ig, ''))
 
-        // Replace variables with fake data
-        .pipe(replace_task({
-            patterns: [{json: fake}],
-            usePrefix: false
-        }))
+          // We need to decode some curly brackets & dollar signs because of MJML
+          .pipe(replace('%7B%7B', '{{'))
+          .pipe(replace('%7D%7D', '}}'))
+          .pipe(replace('&#36;', '$'))
 
-        .pipe(gulp.dest('./dist/html/'))
-});
+          // CSS injection in the header
+          .pipe(replace('</head>', '<style>'+css+'</style></head>'))
 
-// Compile files with MJML
-gulp.task('build:mjml', function () {
-    var css = fs.readFileSync(__dirname+'/src/css/global.css', 'utf8');
+          // Translate
+          .pipe(i18n({
+              langDir: './langs',
+              trace: true,
+              createLangDirs: true
+          }))
 
-    return gulp.src(['src/*.mjml'])
+          // Replace variables with fake data
+          .pipe(replace_task({
+              patterns: [{json: fake}],
+              usePrefix: false
+          }))
 
-        // Compile MJML to HTML
-        .pipe(mjml(mjmlEngine))
+          .pipe(gulp.dest(`./dist/${theme}/html/`))
+  });
 
-        // We need to decode some curly brackets & dollar signs because of MJML
-        .pipe(replace('%7B%7B', '{{'))
-        .pipe(replace('%7D%7D', '}}'))
-        .pipe(replace('&#36;', '$'))
+  // Compile files with MJML
+  gulp.task(`build:mjml:${theme}`, function () {
 
-        // CSS injection in the header
-        .pipe(replace('</head>', '<style>'+css+'</style></head>'))
+      var css = fs.readFileSync(__dirname + `/src/${theme}/css/global.css`, 'utf8');
 
-        // Rename files
-        .pipe(ext_replace('.tpl'))
+      return gulp.src([`src/${theme}/*.mjml`])
 
-        .pipe(gulp.dest('./dist/'+settings.name))
-});
+          // Compile MJML to HTML
+          .pipe(mjml(mjmlEngine))
 
-// Copy images in dist folder
-gulp.task('build:copy:img', function() {
-    return gulp.src('src/img/*.{jpg,jpeg,png,gif}')
-                    .pipe(gulp.dest('./dist/'+settings.name+'/img/'));
-});
+          // We need to decode some curly brackets & dollar signs because of MJML
+          .pipe(replace('%7B%7B', '{{'))
+          .pipe(replace('%7D%7D', '}}'))
+          .pipe(replace('&#36;', '$'))
 
-// Copy tpls in dist folder
-gulp.task('build:copy:tpl', function () {
-        return gulp.src('src/*.tpl')
-            .pipe(gulp.dest('./dist/'+settings.name+'/tpl/'));
-});
+          // CSS injection in the header
+          .pipe(replace('</head>', '<style>'+css+'</style></head>'))
 
-// Copy preview img in dist folder
-gulp.task('build:copy:preview', function() {
-    return gulp.src('src/preview.jpg')
-                    .pipe(gulp.dest('./dist/'+settings.name+'/'));
-});
+          // Rename files
+          .pipe(ext_replace('.tpl'))
 
-// Copy settings in dist folder
-gulp.task('build:copy:settings', function() {
-    return gulp.src('src/config/settings.json')
-                .pipe(gulp.dest('./dist/'+settings.name+'/'));
-});
+          .pipe(gulp.dest(`./dist/${theme}`))
+  });
 
-// Compress folder
-gulp.task('build:compress', ['build:copy:settings', 'build:mjml', 'build:copy:tpl', 'build:copy:img', 'build:copy:preview'], function() {
-    return gulp.src('./dist/'+settings.name+'/**/*')
-                        .pipe(zip(settings.name+'.zip'))
-                        .pipe(gulp.dest('./dist/'));
-});
+  // Copy images in dist folder
+  gulp.task(`build:copy:img:${theme}`, function() {
 
-// Copy images in dist folder
-gulp.task('build', ['build:copy:settings', 'build:mjml', 'build:copy:img', 'build:copy:preview', 'build:copy:tpl', 'build:compress']);
+      return gulp.src(`src/${theme}img/*.{jpg,jpeg,png,gif}`)
+                      .pipe(gulp.dest(`./dist/${theme}/img/`));
+  });
 
-// Watch changes
-gulp.task('watch', function () {
-    gulp.watch('src/**/*.mjml', ['build:dev']);
-    gulp.watch('src/css/global.css', ['build:dev']);
-    gulp.watch('src/config/fake.json', ['build:dev']);
-});
+  // Copy tpls in dist folder
+  gulp.task(`build:copy:tpl:${theme}`, function () {
 
-// Download translations
-gulp.task('langs:dl', ['langs:clean'], function () {
-    ['en', 'fr', 'es'].forEach(function(lang) {
-        download('http://api.addons.prestashop.com/index.php?version=1&method=translations&type=emails&iso_lang='+lang)
+          return gulp.src(`src/${theme}*.tpl`)
+              .pipe(gulp.dest(`./dist/${theme}/tpl/`));
+  });
+
+  // Copy preview img in dist folder
+  gulp.task(`build:copy:preview:${theme}`, function() {
+
+      return gulp.src(`src/${theme}/preview.jpg`)
+                      .pipe(gulp.dest(`./dist/${theme}/`));
+  });
+
+  // Copy settings in dist folder
+  gulp.task(`build:copy:settings:${theme}`, function() {
+
+      return gulp.src(`src/${theme}/config/settings.json`)
+                  .pipe(gulp.dest(`./dist/${theme}/`));
+  });
+
+  // Compress folder
+  gulp.task(`build:compress:${theme}`, function() {
+      return gulp.src(`./dist/${theme}/**/*`)
+                          .pipe(zip(`${theme}.zip`))
+                          .pipe(gulp.dest(`./dist/${theme}`))
+  });
+
+  // Remove dist
+  gulp.task(`build:clean:${theme}`, function () {
+      return del([`dist/${theme}`]);
+  });
+
+  // Copy images in dist folder
+  gulp.task(`build:${theme}`, gulp.series(`build:clean:${theme}`, gulp.parallel(`build:copy:settings:${theme}`, `build:mjml:${theme}`, `build:copy:img:${theme}`, `build:copy:preview:${theme}`, `build:copy:tpl:${theme}`), `build:compress:${theme}`));
+
+  // Watch changes
+  gulp.task(`watch:${theme}`, function () {
+    gulp.watch(`src/${theme}/**/*.mjml`, gulp.series(`build:dev:${theme}`));
+    gulp.watch(`src/${theme}/css/global.css`, gulp.series(`build:dev:${theme}`));
+    gulp.watch(`src/${theme}/config/fake.json`, gulp.series(`build:dev:${theme}`));
+  });
+
+  gulp.task(`mjml:migrate:${theme}`, function () {
+    return gulp.src([`src/${theme}/*.mjml`])
         .pipe(buffer())
-        .pipe(rename("lang.json"))
-        .pipe(gulp.dest('langs/'+lang+'/'));
-    })
+        .pipe(replace(/<(\/?)mj-html/g, '<$1mj-raw'))
+        .pipe(through.obj((file, enc, cb) => {
+                let content = file.contents.toString();
+                if (content.indexOf('<mj-container') >= 0) {
+                    content = htmlBeautify(migrate(content), {
+                        indent_size: 4,
+                        wrap_attributes_indent_size: 4,
+                        end_with_newline: true,
+                    });
+                    file = file.clone();
+                    file.contents = Buffer.from(content);
+                }
+
+                return cb(null, file);
+        }))
+        .pipe(replace(`path="./src/${theme}/`, 'path="./'))
+        .pipe(gulp.dest(`src/${theme}`));
+  });
+
 });
+
+gulp.task('build',
+   gulp.parallel(
+    themes.map(function(theme) { return `build:${theme}` })
+   )
+);
+
+
+gulp.task('watch',
+   gulp.parallel(
+    themes.map(function(theme) { return `watch:${theme}` })
+   )
+);
+
+gulp.task('mjml:migrate',
+   gulp.parallel(
+    themes.map(function(theme) { return `mjml:migrate:${theme}` })
+   )
+);
+
+gulp.task(`clean:all`, function () {
+    return del([`dist/`]);
+});
+
 
 // Remove previously downloaded langs
 gulp.task('langs:clean', function () {
     return del(['langs']);
 });
 
+// Download translations
+gulp.task('langs:dl', gulp.series('langs:clean', function () {
+    return download(['en', 'fr', 'es'].map(lang => ({
+        file: `${lang}/lang.json`,
+        url: `https://api.addons.prestashop.com/index.php?version=1&method=translations&type=emails&iso_lang=${lang}`
+    })))
+        .pipe(gulp.dest(`langs/`));
+}));
+
+
 // Run all tasks if no args
-gulp.task('default', ['watch']);
+gulp.task('default', gulp.series('watch'));
